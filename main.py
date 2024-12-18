1c1fe172a7e2f6b60d975f0f7fd8c3e3fe2bd2d4
jakebourdow <jakebourdow@gmail.com>
2024-12-17 21:11:32 -0800

    create cells

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50,50), Point(400,400))
-    win.draw_line(l)
+    
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(25, 25, 50, 50)
+
+    c = Cell(win)
+    c.draw(50, 25, 75, 50)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(75, 25, 100, 50)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(50, 50, 75, 75)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(50, 25, 75, 0)
+
     win.wait_for_close()
+
+
 
 
 main()
