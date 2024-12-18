90f785770c1e3c852ff2fe482dacb48a1d9d8d8b
jakebourdow <jakebourdow@gmail.com>
2024-12-17 21:48:33 -0800

    clean up class

--- cell.py
+++ cell.py
@@ -13,7 +13,13 @@ class Cell:
         self._y2 = None
         self._win = win
 
-    def draw(self, x1, y1, x2, y2):
+    def draw(
+            self,
+            x1,
+            y1,
+            x2,
+            y2
+        ):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
