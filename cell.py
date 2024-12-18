e46a7cd28d13cbcc03a0b017bb6ca470edbc66de
jakebourdow <jakebourdow@gmail.com>
2024-12-17 21:44:44 -0800

    create solver line

--- cell.py
+++ cell.py
@@ -30,3 +30,18 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = self._x1 + half_length
+        y_center = self._y1 + half_length
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = to_cell._x1 + half_length2
+        y_center2 = to_cell._y1 + half_length2
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -9,12 +9,16 @@ def main():
     c.has_left_wall = False
     c.draw(25, 25, 50, 50)
 
-    c = Cell(win)
-    c.draw(50, 25, 75, 50)
+    c1 = Cell(win)
+    c1.draw(50, 25, 75, 50)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(75, 25, 100, 50)
+    c.draw_move(c1)
+
+    c2 = Cell(win)
+    c2.has_right_wall = False
+    c2.draw(75, 25, 100, 50)
+
+    c2.draw_move(c1, undo=True)
 
     c = Cell(win)
     c.has_bottom_wall = False
