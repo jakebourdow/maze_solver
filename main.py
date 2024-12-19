aba0ca4417d18fce276fe392257f6d14abadd69f
jakebourdow <jakebourdow@gmail.com>
2024-12-18 21:17:07 -0800

    create grid

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 from graphics import Window, Line, Point
 from cell import Cell
+from maze import Maze
 
 
 def main():
-    win = Window(800, 600)
+
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
     
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(25, 25, 50, 50)
-
-    c1 = Cell(win)
-    c1.draw(50, 25, 75, 50)
-
-    c.draw_move(c1)
-
-    c2 = Cell(win)
-    c2.has_right_wall = False
-    c2.draw(75, 25, 100, 50)
-
-    c2.draw_move(c1, undo=True)
-
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(50, 50, 75, 75)
-
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(50, 25, 75, 0)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
 
     win.wait_for_close()
 
